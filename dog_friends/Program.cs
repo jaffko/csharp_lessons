16552d4933f96f3876d9df207c0c198d0d816eaf
Ildar <steel14@yandex.ru>
2022-08-28 22:55:56 +0500

    added dog_friends

---
+++ dog_friends/Program.cs
@@ -0,0 +1,18 @@
+﻿int count = 0, first_friend_speed = 1, second_friend_speed = 2,
+    dog_speed = 5, friend = 2, distance = 10000, time = 0;
+while (distance > 10)
+{
+    if (friend == 2)
+    {
+        time = distance / (second_friend_speed + dog_speed);
+        friend = 1;
+    }
+    else
+    {
+        time = distance / (first_friend_speed + dog_speed);
+        friend = 2;
+    }
+    count++;
+    distance = distance - time * (first_friend_speed + second_friend_speed);
+}
+Console.Write(count);
